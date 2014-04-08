da80546ad98e62c5234aaa7a600faec19f4822ac
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-04-08 16:58:00 +0200

    Huge improvements. ToDo: Fix rotation mess and GUI :)

--- app/src/main/java/cc/locati/cards/app/MainActivity.java
+++ app/src/main/java/cc/locati/cards/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
@@ -15,6 +16,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
--- app/src/main/java/cc/locati/cards/app/highlow/HighlowMainActivity.java
+++ app/src/main/java/cc/locati/cards/app/highlow/HighlowMainActivity.java
@@ -1,50 +1,76 @@
 package cc.locati.cards.app.highlow;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.AlertDialog;
 
+import cc.locati.cards.app.MainActivity;
 import cc.locati.cards.app.R;
 import cc.locati.cards.libs.Card;
 import cc.locati.cards.libs.Deck;
 
-public class HighlowMainActivity extends ActionBarActivity {
+public class HighlowMainActivity extends Activity {
 
     int correctGuesses;
+    int recordScore;
 
-    Deck deck;
+    SharedPreferences sharedPref;
+
+    Deck deck = new Deck();
 
     Card currentCard;
     Card nextCard;
 
     TextView comments;
     TextView points;
+    TextView record;
     TextView currentCardImage;
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_highlow_main);
-
-        deck = new Deck();
-        deck.shuffle();
-        currentCard = deck.dealCard();
-        nextCard = deck.dealCard();
 
         comments = (TextView)findViewById(R.id.textComments);
         points = (TextView)findViewById(R.id.points);
+        currentCardImage = (TextView)findViewById(R.id.card);
+        record = (TextView)findViewById(R.id.record);
 
 /*        SVGImageView currentCardImage = (SVGImageView)findViewById(R.id.imageView);
         currentCardImage.setImageAsset(currentCard.getAsset());*/
-        currentCardImage = (TextView)findViewById(R.id.card);
-        currentCardImage.setText(currentCard.getAsset());
+
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        recordScore = sharedPref.getInt("recordScore", 0);
 
         final Button buttonHigher = (Button) findViewById(R.id.buttonHigher);
         buttonHigher.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 makeChoice('H');
+            }
+        });
+
+        final Button buttonEqual = (Button) findViewById(R.id.buttonEqual);
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                makeChoice('E');
             }
         });
 
@@ -54,41 +80,78 @@ public class HighlowMainActivity extends ActionBarActivity {
                 makeChoice('L');
             }
         });
+
+        drawFirstCard();
     }
 
     private void makeChoice(char choice) {
-        if (nextCard.getValue() == currentCard.getValue()) {
-            comments.setText("The value is the same as the previous card.");
-            comments.setText("You lose on ties.  Sorry!");
+        char rightChoice = 'N';
+        if (nextCard.getValue() > currentCard.getValue())
+            rightChoice = 'H';
+        if (nextCard.getValue() == currentCard.getValue())
+            rightChoice = 'E';
+        if (nextCard.getValue() < currentCard.getValue())
+            rightChoice = 'L';
+        if (choice == rightChoice)
+            rightChoice();
+        else
+            wrongChoice();
+    }
+
+    private void rightChoice() {
+        comments.setText("Your prediction was correct.");
+        correctGuesses++;
+        points.setText(Integer.toString(correctGuesses));
+        drawAnotherCard();
+    }
+
+    private void wrongChoice() {
+        comments.setText("Sorry, next card was " + nextCard.toString());
+
+        if (correctGuesses > recordScore) {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt("recordScore", correctGuesses);
+            editor.commit();
+            recordScore = correctGuesses;
         }
-        else if (nextCard.getValue() > currentCard.getValue()) {
-            if (choice == 'H') {
-                comments.setText("Your prediction was correct.");
-                correctGuesses++;
-                points.setText(Integer.toString(correctGuesses));
-                drawAnotherCard();
-            }
-            else {
-                comments.setText("Your prediction was incorrect.");
-            }
-        }
-        else {  // nextCard is lower
-            if (choice == 'L') {
-                comments.setText("Your prediction was correct.");
-                correctGuesses++;
-                points.setText(Integer.toString(correctGuesses));
-                drawAnotherCard();
-            }
-            else {
-                comments.setText("Your prediction was incorrect.");
-            }
-        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.lost)
+            .setPositiveButton(R.string.tryagain, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    drawFirstCard();
+                }
+            })
+            .setNegativeButton(R.string.quit, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    finish();
+                }
+            });
+
+        AlertDialog alert = builder.create();
+        alert.setCanceledOnTouchOutside(false);
+        alert.setCancelable(false);
+        alert.show();
+    }
+
+    private void drawFirstCard() {
+        record.setText(Integer.toString(recordScore));
+        deck.shuffle();
+        currentCard = deck.dealCard();
+        nextCard = deck.dealCard();
+        currentCardImage.setText(currentCard.getAsset());
+        correctGuesses = 0;
+        points.setText("");
+        comments.setText("");
     }
 
     private void drawAnotherCard() {
-        currentCard = nextCard;
-        nextCard = deck.dealCard();
-        currentCardImage.setText(currentCard.getAsset());
-        comments.setText("");
+        if (deck.cardsLeft() >= 1) {
+            currentCard = nextCard;
+            nextCard = deck.dealCard();
+            currentCardImage.setText(currentCard.getAsset());
+            comments.setText("");
+        } else
+            comments.setText("You won");
     }
 }
