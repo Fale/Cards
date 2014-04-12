4726113a2782bda6f6e17bddb3891700c10be9da
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-04-12 11:04:50 +0100

    Add more tests

--- app/src/main/java/cc/locati/cards/tests/libs/CardTest.java
+++ app/src/main/java/cc/locati/cards/tests/libs/CardTest.java
@@ -13,6 +13,10 @@ public class CardTest extends InstrumentationTestCase {
         Card card;
         card = new Card(5, 0, 0);
         assertEquals(card.toString(), "5 of Hearts");
+        card = new Card();
+        assertEquals(card.toString(), "Joker");
+        card = new Card(1, 1, 0);
+        assertEquals(card.toString(), "Ace of Diamonds");
     }
 
 }
